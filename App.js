657472740b1fec087fdbc0603537ccdd1cce4164
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-07-16 11:23:17 +0300

    navigate between screens

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
-// App.js
-import 'react-native-gesture-handler';
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-elements';
 import HomeScreen from './screens/HomeScreen';
 import CreateReportScreen from './screens/CreateReportScreen';
 
-
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="CreateReport" 
+        component={CreateReportScreen} 
+        options={{ title: 'Create Report' }} 
+      />
+    </Stack.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name="CreateRecord" component={CreateReportScreen } />
-        
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'CreateReport') {
+              iconName = 'file-plus';
+            }
+            return <Icon name={iconName} type="feather" color={color} size={size} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#6200ea',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="CreateReport" component={CreateReportScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
