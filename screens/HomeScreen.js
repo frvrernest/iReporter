f4652bf505fe402eda8258e4eff6aaddc7277bb5
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-07-19 00:27:39 +0300

    Add states and fetch reports.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,48 @@
 import React from "react";
 import { useState, useEffect} from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, Switch, FlatList, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-vector-icons/Ionicons";
 
 const HomeScreen = () => {
+    const [reportType, setReportType] = useState('Red-Flag');
+    const [reports, setReports ] = useState([]);
+    const [viewMap, setViewMap] = useState(false);
+
+    useEffect(() => {
+        fetchReports();
+    },[reportType]);
+
+    const fetchReports = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/reports');
+            const filteredReports = response.data.filter(report => report.type === reportType);
+            setReports(filteredReports);
+        }catch(error) {
+            console.error(error);
+        }
+    }
+
+    const toggleView = () =>setViewMap(!viewMap);
+
     return (
         <View style={styles.container}>
             <View style={styles.topNav}>
-                <Text style={styles.title}>iReporter</Text>
+                
+                <Text style={styles.title}>IReporter</Text>
+                
             </View>
-            <Text>Home Screen</Text>
+
+            <View style={styles.segmentedControl}>
+                <Button title="Red-Flag" onPress={() => setReportType('Red-Flag')}/>
+                <Button title="Intervention" onPress={() => setReportType('Intervention')}/>
+            </View>
+
+            <View style={styles.toggleView}>
+                <Text>List View</Text>
+                <Switch value={viewMap} onValueChange={toggleView} />
+                <Text>Map View</Text>
+            </View>
+
         </View>
     )  
 }
@@ -26,10 +60,20 @@ const styles = StyleSheet.create({
     title: {
         color: '#fff',
         fontSize: 20,
+    },
+    segmentedControl: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginVertical: 10,
+    },
+    toggleView: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10,
       },
 
 
 })
 
 export default HomeScreen;
-
