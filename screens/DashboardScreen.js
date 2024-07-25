8b26626b064e97a4e59f26c22ba71c4174d7a0d3
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-07-26 02:03:22 +0300

    Add fetch reports from db.

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,29 +1,87 @@
 import React from 'react';
+import { useState, useEffect} from "react";
 import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 
+
 const DashboardScreen = () => {
-    
+    const [reports, setReports] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [sortBy, setSortBy] = useState('date');
+    const [filteredReports, setFilteredReports] = useState([]);
+
+    useEffect(() => {
+        fetchReports();
+    },[sortBy]);
+
+    useEffect(() => {
+        filterReports();
+      }, [searchTerm, reports]);
+
+    const fetchReports = async () => {
+        try{
+            const response = await fetch('http://192.168.100.84:3000/reports');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+            const data = await response.json();
+            setReports(data);
+
+        }catch(error){
+            console.error(error);
+        }
+    };
+
+    const filterReports = () => {
+        const filtered = reports.filter(report =>
+          report.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredReports(filtered);
+      };
+
+      const handleSearch = () => {
+        filterReports();
+      };
+
+    const sortedReports = filteredReports.sort((a, b) => {
+    if (sortBy === 'date') {
+        return new Date(b.date) - new Date(a.date);
+    } else if (sortBy === 'status') {
+        return a.status.localeCompare(b.status);
+    } else {
+        return a.location.localeCompare(b.location);
+    }
+    });
+
     return(
         <View style={styles.container}>
             <View style={styles.topNav}>
-                <Text style={styles.title}>IReporter Admin</Text>
+                <Text style={styles.title}> Admin</Text>
+                
             </View>
 
+            <View style={styles.searchContainer}>
             <TextInput
             style={styles.searchBar}
             placeholder="Search reports..."
+            value={searchTerm}
+            onChangeText={setSearchTerm}
             />
+            <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+            </TouchableOpacity>
 
-            <Picker  style={styles.picker}>
+            </View>
+            
+
+            <Picker selectedValue={sortBy} onValueChange={setSortBy} style={styles.picker}>
                 <Picker.Item label="Date" value="date" />
                 <Picker.Item label="Status" value="status" />
                 <Picker.Item label="Location" value="location" />
             </Picker>
 
             <FlatList
-                //data={sortedReports}
+                data={sortedReports}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => (
                 <TouchableOpacity
@@ -51,21 +109,35 @@ const styles=StyleSheet.create({
     },
     topNav: {
         height: 60,
+        flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#6200ee',
+        paddingHorizontal: 20
     },
     title: {
         color: '#fff',
         fontSize: 20,
     },
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 10
+
+    },
     searchBar: {
+        flex:1,
         height: 40,
         borderColor: 'gray',
         borderRadius: 20,
         borderWidth: 1,
         margin: 10,
         paddingLeft: 10,
+      },
+      searchButton: {
+        backgroundColor: '#6200ee',
+        padding: 10,
+        borderRadius: 4,
       },
     picker: {
         height: 50,
