f1c7428581bb05b74caa7c3a8d931b4412e99178
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-07-25 21:43:16 +0300

    Add the top navigation.

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React from 'react';
 import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
+
 const DashboardScreen = () => {
+    return(
+        <View style={styles.container}>
+            <View style={styles.topNav}>
+                <Text style={styles.title}>IReporter Admin</Text>
+            </View>
+        </View>
+    )
 
 }
+const styles=StyleSheet.create({
+    container:{
+        flex:1
+    },
+    topNav: {
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#6200ee',
+    },
+    title: {
+        color: '#fff',
+        fontSize: 20,
+    }
+})
 export default DashboardScreen;
