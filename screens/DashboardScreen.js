e42196110fc7f7b93b80a0e2deb28eab7b582aa1
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-07-25 19:14:24 +0300

    Add necessary imports.

---
+++ screens/DashboardScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+const DashboardScreen = () => {
+
+}
+export default DashboardScreen;
