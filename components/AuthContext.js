0ab09f0367e1b60a96b0ecaa19ddca6293b53414
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-07-28 11:14:02 +0300

    handle tabnavigation appearance on auth

--- components/AuthContext.js
+++ components/AuthContext.js
@@ -1,15 +1,21 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase/firebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-  const login = () => setIsAuthenticated(true);
-  const logout = () => setIsAuthenticated(false);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsSignedIn(!!user);
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isSignedIn }}>
       {children}
     </AuthContext.Provider>
   );
