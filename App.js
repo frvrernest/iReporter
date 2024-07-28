5dac8932496f3819c62d8488684265205ce705b3
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-07-28 11:14:32 +0300

    tab navigator upgrade

--- App.js
+++ App.js
@@ -1,41 +1,35 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import { Avatar } from "react-native-elements";
 import { Icon } from "react-native-elements";
 import CreateReportScreen from "./screens/CreateReportScreen";
-import EditRecordScreen from "./screens/EditRecordScreen";
 import { ReportsProvider } from "./components/ReportsContext";
 import PostedReportsScreen from "./screens/PostedReportsScreen";
 import HomeScreen from "./screens/HomeScreen";
 import Login from "./screens/LoginScreen";
 import SIGNUP from "./screens/SignupScreen";
 import ReportDetailsScreen from "./screens/ReportDetailsScreen";
-
+import MapScreen from "./screens/MapScreen"; 
+import SettingsScreen from "./screens/SettingsScreen"; 
+import FloatingButton from "./components/FloatingButton"; 
 
 // Create stack and tab navigators
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-
 // Function defining the Home stack navigator
 function HomeStack() {
   return (
     <Stack.Navigator>
-      {/* Define the Home screen in the stack navigator */}
       <Stack.Screen
         name="Home"
         component={HomeScreen}
-        // The header is hidden for the HomeScreen by setting headerShown to false.
         options={{ headerShown: false }}
       />
-
       <Stack.Screen name="Login" component={Login} />
-
       <Stack.Screen name="SIGNUP" component={SIGNUP} />
-      {/* Define the PostedReports screen in the stack navigator */}
       <Stack.Screen
         name="PostedReports"
         component={PostedReportsScreen}
@@ -46,64 +40,71 @@ function HomeStack() {
         component={CreateReportScreen}
         options={{ title: "Create Report" }}
       />
-      <Stack.Screen name="EditRecord" component={EditRecordScreen} />
       <Stack.Screen name="ReportDetails" component={ReportDetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function MyReportsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="My Reports"
+        component={PostedReportsScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ReportDetails"
+        component={ReportDetailsScreen}
+        options={{ title: "Report Details" }}
+      />
     </Stack.Navigator>
   );
 }
 
 function App() {
   return (
-    // Wrap the navigation in the ReportsProvider to provide global state
     <ReportsProvider>
       <NavigationContainer>
-        {/* Define the bottom tab navigator */}
-        <Tab.Navigator
-          // Define screen options for the tab navigator
-          screenOptions={({ route }) => ({
-            // Configure tab bar icons based on the route name
-            tabBarIcon: ({ color, size }) => {
-              let iconName;
-              if (route.name === 'Home') {
-                iconName = 'home';
-              } else if (route.name === 'CreateReport') {
-                iconName = 'file-plus';
-              } else if (route.name === 'EditRecord') {
-                iconName = 'edit';
-              }
-              // Return the icon component from react-native-elements
-              return (
-                <Icon
-                  name={iconName}
-                  type="feather"
-                  color={color}
-                  size={size}
-                />
-              );
-            },
-          })}
-          // Define tab bar appearance options
-          tabBarOptions={{
-            
-              backgroundColor: '#333', // Dark background color
-            
-            // Color of the active tab icon
-            activeTintColor: "#6200ea",
-            inactiveTintColor: "black",
-          }}
-        >
-          {/* 
-            The Home tab uses the HomeStack navigator.
-            The CreateReport tab directly uses the CreateReportScreen.
-          */}
-          <Tab.Screen
-            name="Home"
-            component={HomeStack}
-            options={{ headerShown: false }}
-          />
-          <Tab.Screen name="CreateReport" component={CreateReportScreen} />
-          <Tab.Screen name="EditRecord" component={EditRecordScreen} />
-        </Tab.Navigator>
+        <View style={{ flex: 1 }}>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+                if (route.name === 'Home') {
+                  iconName = 'home';
+                } else if (route.name === 'My Reports') {
+                  iconName = 'file-text';
+                } else if (route.name === 'Map') {
+                  iconName = 'map';
+                } else if (route.name === 'Settings') {
+                  iconName = 'settings';
+                } else if (route.name === 'CreateReport') {
+                  iconName = 'camera';
+                }
+                return (
+                  <Icon
+                    name={iconName}
+                    type="feather"
+                    color={color}
+                    size={size}
+                  />
+                );
+              },
+            })}
+            tabBarOptions={{
+              backgroundColor: '#333', 
+              activeTintColor: "#6200ea",
+              inactiveTintColor: "black",
+            }}
+          >
+            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="My Reports" component={MyReportsStack} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+          <FloatingButton />
+        </View>
       </NavigationContainer>
     </ReportsProvider>
   );
